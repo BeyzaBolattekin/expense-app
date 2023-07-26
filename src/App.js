30b8f10801f02a998ff85eb09a9c1548eb06af96
Beyza Bolattekin <beyzabolattekin@Beyzas-MacBook-Air.local>
2023-07-26 15:00:35 +0300

    created a function to crate expense items, moved createExpenseItems func out of App func

--- src/App.js
+++ src/App.js
@@ -2,34 +2,34 @@ import "./App.css";
 import "./components/ExpenseItem";
 import ExpenseItem from "./components/ExpenseItem";
 
-const expenses = [
-  {
-    id: "e1",
-    title: "Toilet Paper",
-    amount: 94.12,
-    date: new Date(2020, 7, 14),
-  },
-  {
-    id: "e2",
-    title: "New TV",
-    amount: 799.49,
-    date: new Date(2021, 2, 12),
-  },
-  {
-    id: "e3",
-    title: "Car Insurance",
-    amount: 294.67,
-    date: new Date(2021, 2, 28),
-  },
-  {
-    id: "e4",
-    title: "New Desk (Wooden)",
-    amount: 450,
-    date: new Date(2021, 5, 12),
-  },
-];
+function createExpenseItems() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    {
+      id: "e2",
+      title: "New TV",
+      amount: 799.49,
+      date: new Date(2021, 2, 12),
+    },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
 
-function App() {
   const expenseItems = expenses.map((expense) => (
     <ExpenseItem
       title={expense.title}
@@ -37,7 +37,11 @@ function App() {
       date={expense.date}
     ></ExpenseItem>
   ));
+  return expenseItems;
+}
 
+function App() {
+  const expenseItems = createExpenseItems();
   return <div>{expenseItems}</div>;
 }
 
